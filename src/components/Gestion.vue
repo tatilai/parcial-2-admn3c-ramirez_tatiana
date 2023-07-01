589f89393d0e6ef32147e3882fc9c8d0f077ab39
tatilai <99092149+tatilai@users.noreply.github.com>
2023-07-01 14:05:21 -0300

    componente gestion

--- src/components/Gestion.vue
+++ src/components/Gestion.vue
@@ -1,17 +1,61 @@
 <template>
-  <div>
-    <h1>Gestión</h1>
-   
-  </div>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" sm="6" md="3">
+          <v-text-field v-model="nombreProyecto" label="Nombre del proyecto"></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6" md="3">
+          <v-text-field v-model="responsable" label="Responsable"></v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-container fluid>
+            <v-textarea v-model="descripcion" autocomplete="explicacion del proyecto" label="Descripción"></v-textarea>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <div class="text-center">
+      <v-btn
+        rounded
+        color="primary"
+        dark
+      >
+        Continuar
+      </v-btn>
+    </div>
+
+  </v-form>
 </template>
 
 <script>
 export default {
   name: 'Gestion',
-  
+  data() {
+    return {
+      nombreProyecto: '',
+      responsable: '',
+      descripcion: '',
+    };
+  },
 };
 </script>
 
 <style scoped>
 
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.text-field {
+  width: 100%;
+  margin-bottom: 20px;
+}
+
+.text-area {
+  width: 100%;
+  margin-bottom: 20px;
+}
 </style>
