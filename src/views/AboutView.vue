4a97cfba6b5af15b177e5e9f9f7333b79c507196
tatilai <99092149+tatilai@users.noreply.github.com>
2023-07-02 16:53:57 -0300

    agrego imagenes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -119,15 +119,17 @@
 
 
      <v-card
-    class="mx-auto"
+    class="mx-auto mt-5 mb-5"
     max-width="400"
+
   >
     <v-img
       class="white--text align-end"
       height="200px"
-      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+     :src="require('@/assets/lista-de-verificacion_2.png')"
+    
     >
-      <v-card-title>Info del formulario enviado</v-card-title>
+      <v-card-title style="color:black">Info del formulario enviado</v-card-title>
     </v-img>
 
     <v-card-subtitle class="pb-0">
@@ -153,21 +155,7 @@
 
     </v-card-text>
 
-    <v-card-actions>
-      <v-btn
-        color="orange"
-        text
-      >
-        Share
-      </v-btn>
-
-      <v-btn
-        color="orange"
-        text
-      >
-        Explore
-      </v-btn>
-    </v-card-actions>
+   
   </v-card>
        
     
