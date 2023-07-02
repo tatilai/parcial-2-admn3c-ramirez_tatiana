f80a281c2f52ea5dea78831e550b1d5eedeb2d62
tatilai <99092149+tatilai@users.noreply.github.com>
2023-07-01 22:20:51 -0300

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -58,7 +58,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'HelloWorld',
   props: {
@@ -83,33 +82,15 @@ export default {
     validate() {
       this.$refs.form.validate()
     },
-    reset() {
-      this.$refs.form.reset()
-    },
-    resetValidation() {
-      this.$refs.form.resetValidation()
-    },
+    
   },
 }
-
-
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.formularioUno{
+   margin: 0 auto;
+  max-width: 800px;
 }
 </style>
