00e8f4e65645fb77dd8fe04274a9c3c3385bd833
tatilai <99092149+tatilai@users.noreply.github.com>
2023-07-02 14:30:07 -0300

    componete formulario

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,7 +88,7 @@
     
 
    
-<!-- <v-dialog v-model="dialog" max-width="500px">
+     <!-- <v-dialog v-model="dialog" max-width="500px">
       <v-card>
         <v-card-title class="text-h5">Editar Proyecto</v-card-title>
         <v-card-text>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,42 +3,134 @@
    <p class="font-weight-bold w-400 display-1 text-center my-4">
       Formulario de contacto entre los equipos
     </p>
-      <v-card
-    class="mx-auto"
-    :width="customWidth"
-    :height="customHeight"
-    color="#FFAB91"
-    outlined
-    >
-      <p class="font-weight-medium my-5 mx-4">
-      Aqui tienes el espacio para dejar un mensaje a los miembros de tu equipo sobre lo que te parecio el proyecto y si le deseas hacer cambios
+
+     <v-card class="mx-auto" :width="customWidth" color="#FFAB91" outlined>
+      <v-row> 
+      <v-col cols="6">      
+          <v-card-text>
+            <p class="font-weight-medium my-5 mx-4">
+              Aquí tienes el espacio para dejar un mensaje a los miembros de tu equipo sobre lo que te pareció el proyecto y si le deseas hacer cambios
+            </p>
+         </v-card-text>
+         </v-col>
+         <v-col cols="6"> 
+            <v-img
+              :src="require('@/assets/trabajo.png')"
+              alt="gente sentada"
+              :max-width="355"
+              :max-height="289"
+              class="my-4 v-image__image--preload"
+            ></v-img>
+          </v-col>
+        </v-row>   
+    
+
+       
+
+
+      <v-form  
+   ref="form"
+   v-model="valid"
+   lazy-validation
+   class="mx-10">
+     <v-text-field
+      v-model="nombreProyecto"
+      :rules="nameRules"      
+      label="Ingrese nombre del equipo"
+      required     
+    ></v-text-field>
+    <v-text-field
+      v-model="responsableProyecto"
+      :rules="emailRules"
+      label="Ingrese su telefono"
+      required      
+    ></v-text-field>
+
+     <v-text-field
+      v-model="responsableProyecto"
+      :rules="emailRules"
+      label="Ingrese su e-mail"
+      required      
+    ></v-text-field>
+
+        <v-select
+      v-model="select"
+      :items="items"
+      :rules="[v => !!v || 'Item is required']"
+      label="Indique qué rol desempeña en el equipo"
+      required
+    ></v-select>
+
+    <v-container fluid>
+      <v-textarea
+      v-model="descripcionProyecto"
+       :rules="descriptionRules"        
+       label="Comentarios"
+      ></v-textarea>
+    </v-container>
+
+     <v-btn
+      class="mr-4 my-4"
+      @click="continuarProyecto"
+      color="#FFE082">
+      Continuar
+    </v-btn>
+  </v-form>
+  </v-card>
+ 
+
+
+     <p class="font-weight-bold w-400 display-1 text-center my-4">
+      Lista de Proyectos
     </p>
 
+    <v-simple-table dark class="my-20">
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">
+          Nombre del equipo
+          </th>
+          <th class="text-left">
+          Telefono
+          </th>
+          <th class="text-left">
+           e-mail
+          </th>  
+          <th class="text-left">
+          comentarios
+          </th>     
+         </tr>       
+        </thead>         
+     
 
-    <v-img
-    :src="require('@/assets/trabajo.png')"
-    alt="gente sentada"
-    :max-width="355"
-    :max-height="289"   
-    class="my-4 v-image__image--preload"
-    ></v-img>
 
+      <tbody>
+          <tr v-for="proyecto in listaProyectos" :key="proyecto.nombreProyecto">
+            <td>{{ proyecto.nombreProyecto }}</td>
+            <td>{{ proyecto.descripcionProyecto }}</td>
+            <td>{{ proyecto.responsableProyecto }}</td>
+           
+          </tr>
+        </tbody>
+     </template>
+       </v-simple-table>    
+  
+       
+    
 
-
-    <v-card-actions>
-      <v-spacer></v-spacer>
-      <v-btn
-        outlined
-        rounded
-        text
-        style="margin-right:30px"       
-      >
-        Button
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+ 
   </div>
-</template>
+</template>  
+
+   
+
+
+   
+
+
+
+ 
 
 
 <script>
